4544db399eae52467c90f6c94aa11d002c6c8e6c
Sesha_Papolu <Sesha_Papolu@EPINHYDW1014>
2017-04-25 20:55:17 +0530

    Added Startup File

---
+++ KatanaImplementation/KatanaImplementation/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace KatanaImplementation
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                Debug.WriteLine("Incoming Request" + ctx.Request.Path);
+                await next();
+                Debug.WriteLine("Outgoing Request" + ctx.Request.Path);
+            });
+
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("<html><head></head><body>hello seshu</body><html>");
+            });
+
+        }
+    }
+}
