18429fb66f2493672d83f1999cc721835819aa0c
Sesha_Papolu <Sesha_Papolu@EPINHYDW1014>
2017-04-27 15:53:30 +0530

    Removed Comments

--- KatanaImplementation/KatanaImplementation/Startup.cs
+++ KatanaImplementation/KatanaImplementation/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using KatanaImplementation.Middlewares;
 using Owin;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
